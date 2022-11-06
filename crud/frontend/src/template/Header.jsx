53b31fd7afc52617eff03c92a956017630e7b998
Camis <Camis@MacBook-Pro.local>
2022-11-06 16:22:50 -0300

    add: frontend  and gitignore

---
+++ crud/frontend/src/template/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css'
+import React from 'react'
+
+export default props =>{
+
+
+    <header className="header">
+        Cabeçalho
+    </header>
+}
---
+++ crud/frontend/src/template/Nav.jsx
@@ -0,0 +1,8 @@
+//import ' ./Nav.css'
+import React from 'react'
+
+export default props =>{
+    <nav className="nav">
+        Navegaçao
+    </nav>
+}
